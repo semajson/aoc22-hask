b81e06b8f03cff29ad48c25dfad5c43541695bd7
james sandy <jamessandy95@gmail.com>
2022-12-04 17:11:28 +0000

    day01 test pass

--- day01/day01.hs
+++ day01/day01.hs
@@ -1,10 +1,22 @@
 import Data.List
+import Data.List.Split
 import System.IO
 
--- parse:: String -> [String]
--- parse =
+
+toInt :: [String] -> [Int]
+toInt = map read
+
+parse :: [String] -> [[Int]]
+parse x = map toInt (splitWhen (== "") x)
+
+solve :: [[Int]] -> Int
+solve test = maximum totalCals
+    where totalCals = map sum test
+
 
 main = do
     input <- readFile "test_inputs.txt"
     let input_lines =lines input
-    print input_lines
+    let parsed_data = parse input_lines
+    let sol = solve parsed_data
+    print sol
