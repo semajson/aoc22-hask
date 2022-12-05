fbf1db1cb1939ce3c750d1013431d87769ad7bb8
james sandy <jamessandy95@gmail.com>
2022-12-05 23:37:18 +0000

    part 2 works

--- day02/Day02.hs
+++ day02/Day02.hs
@@ -2,9 +2,11 @@
 import           Data.List
 import           Data.List.Split
 import           Data.List.Split
+import           Data.Maybe
 
 type Line = String
 type Shape = String
+type Result = Int
 
 parse :: [Line] -> [[Shape]]
 parse x = map (splitOn " ") x
@@ -24,11 +26,17 @@ shapeScore "P" = 2
 shapeScore "S" = 3
 shapeScore _   = error "unexpected shape"
 
-shapeIndex :: Shape -> Int
-shapeIndex "R" = 0
-shapeIndex "P" = 1
-shapeIndex "S" = 2
-shapeIndex _   = error "unexpected shape"
+shapeToIndex :: Shape -> Int
+shapeToIndex "R" = 0
+shapeToIndex "P" = 1
+shapeToIndex "S" = 2
+shapeToIndex x   = error ( "unexpected shape: " ++ x)
+
+indexToShape :: Int -> Shape
+indexToShape  0 = "R"
+indexToShape  1 = "P"
+indexToShape  2 = "S"
+indexToShape _  = error "unexpected shape"
 
 myShapeScore:: [Shape] -> Int
 myShapeScore [_, myShape] = shapeScore (decodeShape myShape)
@@ -38,22 +46,44 @@ resultMatrix = [[3, 6, 0],
                 [0, 3, 6],
                 [6, 0, 3]]
 
-roundResult :: [Shape] -> Int
-roundResult [encOppShape, encMyShape] = (resultMatrix !! (shapeIndex oppShape)) !!(shapeIndex myShape)
+roundResult1 :: [Shape] -> Result
+roundResult1 [encOppShape, encMyShape] = (resultMatrix !! (shapeToIndex oppShape)) !!(shapeToIndex myShape)
     where oppShape = decodeShape encOppShape
           myShape = decodeShape encMyShape
-roundResult _ =  error "unexpected"
+roundResult1 _ =  error "unexpected"
 
-calcRoundScore:: [Shape] -> Int
-calcRoundScore round = (roundResult round) + (myShapeScore round)
+calcRoundScore1:: [Shape] -> Int
+calcRoundScore1 round = (roundResult1 round) + (myShapeScore round)
+
 
 solve1 :: [[Shape]] -> Int
-solve1 stratergyGuide = sum (map calcRoundScore stratergyGuide)
+solve1 stratergyGuide = sum (map calcRoundScore1 stratergyGuide)
+
+decodeResult :: Shape -> Result
+decodeResult "Y" = 3
+decodeResult "X" = 0
+decodeResult "Z" = 6
+decodeResult _   = error "unexpected shape"
+
+
+getMyShape2:: Result -> Shape -> Shape
+getMyShape2 result oppShape = indexToShape(  fromJust (elemIndex result myShapeIndexOutcomes))
+    where myShapeIndexOutcomes = resultMatrix!!(shapeToIndex oppShape)
+
+calcRoundScore2:: [Shape] -> Int
+calcRoundScore2 [encOppShape, encResult] = result + (shapeScore (getMyShape2 result oppShape))
+    where result = decodeResult encResult
+          oppShape = decodeShape encOppShape
+
+
+solve2 :: [[Shape]] -> Int
+solve2 stratergyGuide = sum (map calcRoundScore2 stratergyGuide)
+
 
 main:: IO()
 main = do
     input <- readFile "real_inputs.txt"
     let input_lines =lines input
     let parsed_data = parse input_lines
-    let sol = solve1 parsed_data
+    let sol = solve2 parsed_data
     print sol
